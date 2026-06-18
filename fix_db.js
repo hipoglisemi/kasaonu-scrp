47a63ed656ee88aea86758354759c4f91f54ded4
Oguz Karaevli <oguz.karaevli@gmail.com>
2026-06-18 14:14:02 +0300

    cleanup: remove obsolete and temporary scripts from root directory

--- fix_db.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgres://postgres:OKaNWkuA52DaZoaTsGm6gCCqTgk03W9PXsFIWsc77NhTAGwZID3wqOel58mkOsBB@localhost:5434/postgres' });
-client.connect().then(async () => {
-  await client.query(`ALTER TABLE "test_campaigns" ADD COLUMN IF NOT EXISTS "cards_audited_at" TIMESTAMP;`);
-  console.log("Added cards_audited_at to test_campaigns");
-  client.end();
-}).catch(e => console.error(e));
