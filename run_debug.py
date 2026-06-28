17a969597a6f9a859cd775345611e8f53770f8bb
Oguz Karaevli <oguz.karaevli@gmail.com>
2026-06-28 12:47:23 +0300

    chore: remove old debug and test files

--- run_debug.py
+++
@@ -1,5 +0,0 @@
-import requests
-from debug_cleaner_temp import clean_campaign_text
-url = "https://www.adioscard.com.tr/kampanyalar/silver-logolu-mastercardiniza-ozel-galataportta-otopark-1-tl"
-html = requests.get(url, verify=False).text
-clean_campaign_text(html, title="Silver logolu Mastercard’ınıza özel Galataport’ta otopark 1 TL!")
--- test_crystal.py
+++
@@ -1,7 +0,0 @@
-import requests
-from src.services.text_cleaner import clean_campaign_text
-url = "https://www.crystalcard.com.tr/kampanyalar/vakkoda-pesin-fiyatina-6-taksit"
-html = requests.get(url, verify=False).text
-cleaned = clean_campaign_text(html, title="Vakko’da peşin fiyatına 6 taksit!")
-print("--- FULL CLEANED TEXT ---")
-print(cleaned)
