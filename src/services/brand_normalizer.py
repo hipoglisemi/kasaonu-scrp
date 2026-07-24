1df53c17793bb1b7de2c0d9ba68be574b85e95bd
Oguz Karaevli <oguz.karaevli@gmail.com>
2026-07-24 21:23:06 +0300

    Add whatsapp to forbidden brand names in normalizer

--- src/services/brand_normalizer.py
+++ src/services/brand_normalizer.py
@@ -103,7 +103,7 @@ def cleanup_brands(brand_input):
         'juzdan', 'jüzdan', 'bonusflaş', 'bonusflas', 'ayrıcalık', 'avantaj', 'pos', 'üye işyeri', 'üye iş yerleri',
         'mobilya', 'sigorta', 'nalburiye', 'kozmetik', 'akaryakıt', 'giyim', 'aksesuar', 'elektronik', 'market', 'gıda',
         'restoran', 'kafe', 'e-ticaret', 'ulaşım', 'turizm', 'konaklama', 'otomotiv', 'kamu', 'eğitim',
-        'genel', 'yok', 'null', 'happy'
+        'genel', 'yok', 'null', 'happy', 'whatsapp'
     ]
     
     final_brands = []
